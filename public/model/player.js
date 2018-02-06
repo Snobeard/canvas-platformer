11686415457bd4b8e128a2aef306d166479e8c04
Snobeard <fatmattmatt@gmail.com>
2018-02-06 00:52:52 -0800

    character fully animated

--- public/model/player.js
+++ public/model/player.js
@@ -187,10 +187,21 @@ class Player {
       ],
     };
 
-    let fallingCharacter = [
-      215, // image: x - location  
-      40, // image: y - location
-      13, // image: x - span
+    let parachute = [
+      214, // image: x - location  
+      10, // image: y - location
+      18, // image: x - span
+      16, // image: y - span
+      this.x - 5, // image: x- positioning
+      this.y - 25, // image: y - positioning
+      this.width + 10, // image: width
+      this.height + 6  // image: height
+    ];
+
+    let crouchingCharacter = [
+      216, // image: x - location  
+      101, // image: y - location
+      12, // image: x - span
       16, // image: y - span
       this.x, // image: x- positioning
       this.y, // image: y - positioning
@@ -198,23 +209,42 @@ class Player {
       this.height + 2  // image: height
     ];
 
+    let jumpingCharacter = [
+      92, // image: x - location  
+      10, // image: y - location
+      14, // image: x - span
+      18, // image: y - span
+      this.x, // image: x- positioning
+      this.y, // image: y - positioning
+      this.width, // image: width
+      this.height + 2  // image: height
+    ];
 
-    // // mattL - if the velocity is greater than 0.5 
-    // //         then the player is moving at a noticeable rate
-    // if (Math.abs(this.velX) > 0.5) {
-    //   this.characterFrame += 1;
-    //   // mattL - increments the movingCharacter Frame Sequence to make the
-    //   //         character look like it's moving. '/ 5' sets the animation speed
-    //   let frame = Math.floor(this.characterFrame / 5);
-    //   // mattL - '(% 6) + 1' selects the character frame
-    //   //         ex: sequence 1 through 6
-    //   this.drawCharacter(movingCharacter[(frame % 6) + 1]);
-    // } else {
-    //   this.characterFrame = 0;
-    //   this.drawCharacter(standingCharacter);
-    // }
 
-    this.drawCharacter(fallingCharacter);
+    if (this.velY < 0) {
+      this.drawCharacter(jumpingCharacter);
+    }
+    else if (this.falling && this.crouching) {
+        this.drawCharacter(parachute);
+        this.drawCharacter(standingCharacter);
+    }
+    else if (this.crouching) {
+      this.drawCharacter(crouchingCharacter)
+    }
+    // mattL - if the velocity is greater than 0.5 
+    //         then the player is moving at a noticeable rate
+    else if (Math.abs(this.velX) > 0.5) {
+      this.characterFrame += 1;
+      // mattL - increments the movingCharacter Frame Sequence to make the
+      //         character look like it's moving. '/ 5' sets the animation speed
+      let frame = Math.floor(this.characterFrame / 5);
+      // mattL - '(% 6) + 1' selects the character frame
+      //         ex: sequence 1 through 6
+      this.drawCharacter(movingCharacter[(frame % 6) + 1]);
+    } else {
+      this.characterFrame = 0;
+      this.drawCharacter(standingCharacter);
+    }
   }
 
   setDirection() {
